e5ca4459bc859119aa794f7fd5859b07c5637117
avulanitheesh <ee24mt032@iitdh.ac.in>
2024-09-22 20:54:50 +0530

    add some more ports and finished init function

--- main.c
+++ main.c
@@ -23,4 +23,13 @@ void init(void) {
     GPIO_PORTF_DIR_R = 0x02;         // Set PF1 as output (LED)
     GPIO_PORTF_DEN_R = 0x12;         // Digital enable for PF1 and PF4
     GPIO_PORTF_PUR_R |= 0x10;        // Enable pull-up resistor for PF4
+ GPIO_PORTF_IS_R = 0x00;          // Edge-sensitive
+    GPIO_PORTF_IBE_R = 0x00;         // Interrupt controlled by IEV
+    GPIO_PORTF_IEV_R = 0x00;         // Falling edge trigger
+    GPIO_PORTF_ICR_R = 0x10;         // Clear prior interrupt
+    GPIO_PORTF_IM_R = 0x10;          // Unmask interrupt for PF4
+
+    NVIC_EN0_R = 0x40000000;         // Enable interrupt in NVIC
+}
+
 
